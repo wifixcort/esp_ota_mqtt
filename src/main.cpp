f1bfdf53ca9c3fbd1b589e43226885b77fad2377
Ricardo Mena C <rgacort12@gmail.com>
2020-07-15 21:11:37 -0600

    Update Adafruit MQTT & configs file
    Signed-off-by: Ricardo Mena C <rgacort12@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,44 @@
+/*
+ESP8266 MQTT with OTA
+This skech is in charge to verify and maintain the MQTT connection with the server
+
+Ricardo Mena C.
+https://wfx.ddns.net
+
+License
+**********************************************************************************
+This program is free software; you can redistribute it
+and/or modify it under the terms of the GNU General
+Public License as published by the Free Software
+Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will
+be useful, but WITHOUT ANY WARRANTY; without even the
+implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public
+License for more details.
+
+You should have received a copy of the GNU General
+Public License along with this program.
+If not, see <http://www.gnu.org/licenses/>.
+
+Licence can be viewed at
+http://www.gnu.org/licenses/gpl-3.0.txt
+
+Please maintain this license information along with authorship
+and copyright notices in any redistribution of this code
+**********************************************************************************
+*/
+
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
-#include "psswd.h" //Rename psswd.h.default to psswd.h and fill it with your personal data
+#include "configs.h" //MUST: Rename configs.h.default to configs.h and fill it with your personal data
 
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
-
-
-#define MQTT_SERVER      "m13.cloudmqtt.com"
-#define MQTT_SERVERPORT  12460                  // use 8883 for SSL
 
 // Create an ESP8266 WiFiClient class to connect to the MQTT server.
 WiFiClient client;
@@ -17,7 +46,7 @@ WiFiClient client;
 // Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
 Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_PSSWD);
 
-Adafruit_MQTT_Publish status = Adafruit_MQTT_Publish(&mqtt, "test_in");//Test topic
+Adafruit_MQTT_Publish status = Adafruit_MQTT_Publish(&mqtt, TOPIC_N1);//Test topic
 
 uint32_t previousMillis = 0;        // will store last time LED was updated
 uint32_t prevMillis = 0;
@@ -49,9 +78,9 @@ void setup() {
   // ArduinoOTA.setPort(8266);
 
   // Hostname defaults to esp8266-[ChipID]
-  ArduinoOTA.setHostname("wifixOTA");
+  ArduinoOTA.setHostname(HOSTNAME);
 
-  // No authentication by default
+  // Authentication by OTA_PSSWD
   ArduinoOTA.setPassword(OTA_PSSWD);
 
   //Gamma functions for events
@@ -85,7 +114,7 @@ void setup() {
 void loop() {
   ArduinoOTA.handle();//Wifi service handler
   blink(millis(), 1000);//Blink LED every 1s
-  stats(millis(), 10000);//Send analog reads every 10s
+  stats(millis(), 10000);//Send analog reads every 10s to MQTT Server
   MQTT_connect();//Stablish connection with mqtt server
   if(WiFi.status() != WL_CONNECTED){//Continuously check wifi status
     ESP.restart();
